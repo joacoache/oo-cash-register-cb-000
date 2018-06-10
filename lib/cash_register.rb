15a33e95dbbe8604ad544f6be816a56a80b05d9c
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-10 19:53:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def cash_register
+    total = self.new
+    total.total = 0
+  end
+
+  def cash_register_with_discount(discount)
+    total = self.new
+    total.total = 0
+    @discount = discount
+  end
+
+end
