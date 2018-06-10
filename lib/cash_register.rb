f33033e91c5279da1285378f15a8096682b6c3f4
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-10 20:33:20 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    @total += price * quatity
+    @total += price * quantity
     quantity.times do
       @array << title
     end
